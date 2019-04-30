be11858ae42ca5077dbd4bb4495a7a171b369a46
Haichao <haichaozhu77@gmail.com>
2019-04-30 18:49:40 +0800

    add copy button to code block

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,80 +12,90 @@
 const users = [];
 
 const siteConfig = {
-  title: 'Nervos CKB', // Title for your website.
-  tagline: 'Nervos CKB Documentations',
-  url: 'https://docs.nervos.org', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+    title: 'Nervos CKB', // Title for your website.
+    tagline: 'Nervos CKB Documentations',
+    url: 'https://docs.nervos.org', // Your website URL
+    baseUrl: '/', // Base URL for your project */
+    // For github.io type URLs, you would set the url and baseUrl like:
+    //   url: 'https://facebook.github.io',
+    //   baseUrl: '/test-site/',
 
-  // Used for publishing and more
-  projectName: 'nervos-docs',
-  organizationName: 'Nervos',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+    // Used for publishing and more
+    projectName: 'nervos-docs',
+    organizationName: 'Nervos',
+    // For top-level user or org sites, the organization is still the same.
+    // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+    //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [ 
-    { href: 'https://github.com/nervosnetwork', label: 'GitHub', external: true }
-      
-  ],
+    // For no header links in the top nav bar -> headerLinks: [],
+    headerLinks: [{
+            href: 'https://github.com/nervosnetwork',
+            label: 'GitHub',
+            external: true
+        }
 
-  // If you have users set above, you add it here:
-  users,
-
-  /* path to images for header/footer */
-  headerIcon: 'img/nervos-logo.png',
-  favicon: 'img/favicon.ico',
-
-  /* Colors for website */
-  colors: {
-    primaryColor: '#11356f',
-    secondaryColor: '#4bbc8e',
-  },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
     ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
+    // If you have users set above, you add it here:
+    users,
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+    /* path to images for header/footer */
+    headerIcon: 'img/nervos-logo.png',
+    favicon: 'img/favicon.ico',
 
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
+    /* Colors for website */
+    colors: {
+        primaryColor: '#11356f',
+        secondaryColor: '#4bbc8e',
+    },
 
-  // Collapsible Categories
-  docsSideNavCollapsible: true,
+    /* Custom fonts for website */
+    /*
+    fonts: {
+      myFont: [
+        "Times New Roman",
+        "Serif"
+      ],
+      myOtherFont: [
+        "-apple-system",
+        "system-ui"
+      ]
+    },
+    */
+
+    highlight: {
+        // Highlight.js theme to use for syntax highlighting in code blocks.
+        theme: 'default',
+    },
+
+    // Add custom scripts here that would be placed in <script> tags.
+    scripts: [
+        'https://buttons.github.io/buttons.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+        '/js/code-block-buttons.js',
+    ],
+
+    // Add custom style sheets here
+    stylesheets: ['/css/code-block-buttons.css'],
+
+    // On page navigation for the current documentation page.
+    onPageNav: 'separate',
+    // No .html extensions for paths.
+    cleanUrl: true,
+
+    // Collapsible Categories
+    docsSideNavCollapsible: true,
 
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+    // Show documentation's last contributor's name.
+    // enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+    // Show documentation's last update time.
+    // enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    // You may provide arbitrary config keys to be used as needed by your
+    // template. For example, if you need your repo's URL...
+    //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
