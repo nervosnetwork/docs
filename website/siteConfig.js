b3992cc8b73d256f82af40d0f6ecd17f5b54aaa3
Mine77 <haichaozhu77@gmail.com>
2019-05-09 13:48:44 +0800

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,16 +14,18 @@ const users = [];
 const siteConfig = {
     title: 'Nervos CKB', // Title for your website.
     tagline: 'Nervos CKB Documentations',
-    url: 'https://docs.nervos.org', // Your website URL
+    url: 'https://docs.staging.nervos.org', // Your website URL
     baseUrl: '/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     docsUrl: '',
+    
+    scrollToTop:true,
 
     // Used for publishing and more
-    projectName: '',
+    projectName: 'docs',
     organizationName: 'Nervos',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
